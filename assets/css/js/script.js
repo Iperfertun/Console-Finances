cea66b865c52c509707ca4263975a67037b2c6b9
Tunde <Foltunde@gmail.com>
2024-01-03 00:26:42 +0000

    create the javascript

---
+++ assets/css/js/script.js
@@ -0,0 +1,14 @@
+let Months = ['January','February','March','April','May','June',
+                'July','August','September','October','November','December'];
+let myHtml = '<ol>';
+Total Mo: 86
+Total: $38382578
+Average Change: -2315.12
+Greatest Increase in Profits/Losses: Feb-2012 ($1926159)
+Greatest Decrease in Profits/Losses: Sep-2013 ($-2196167)v
+
+for (let i = 0; i < months.length; i++) {
+  myHtml += `<li>${fruits[i]}</li>`;
+}
+myHtml += '</ol>';
+console.log(myHtml);
